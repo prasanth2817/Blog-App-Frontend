668b6619f090afda26c1136bc6c2f1a1792d5bf8
Prasanth k <prasanth2817@gmail.com>
2025-05-18 18:18:29 +0530

    fix: correct usage of Navigate component for route redirection

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "../Contexts/AuthContext";
 import Login from "../Pages/Login";
@@ -6,23 +6,23 @@ import SignUp from "../Pages/SignUp";
 import Home from "../Pages/Home";
 
 function AppRoutes() {
-    const Navigate = useNavigate();
   const { authUser } = useAuthContext();
+
   return (
     <>
-
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/home" element={<Home />} />
-          <Route
-            path="/"
-            element={authUser ? <Home /> : <Navigate to={"/login"} />}
-          />
-        </Routes>
-        <Toaster />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<Home />} />
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
+        />
+      </Routes>
+      <Toaster />
     </>
   );
 }
 
 export default AppRoutes;
+
